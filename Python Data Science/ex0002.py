dc9b18035217fd3eec4f3b91fd88282ec4281322
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-09-25 17:57:39 -0300

    25-09-23

---
+++ Python Data Science/ex0002.py
@@ -0,0 +1,14 @@
+#Calculadora em python
+
+n1 = int(input("Digite um numero"))
+n2 = int(input("Digite outro numero"))
+
+res = n1 + n2
+print(res)
+res = n1 - n2
+print(res)
+res = n1 * n2
+print(res)
+res = n1 / n2
+print(res)
+
---
+++ Python Data Science/ex0003.py
@@ -0,0 +1,4 @@
+nome = input("Digite o seu nome:")
+idade = input("Digite a sua idade:")
+altura = input("Digite a sua altura em m:")
+print("Olá, "+nome+", você tem "+idade+" anos e"+" mede "+altura+" metros!")
