285a72bb5e4a68bd215439ec09bb49079330f33a
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-09-25 20:25:36 -0300

    25-09-23

--- Hashtag Treinamentos - Python/AULA1/codigo.py
+++ Hashtag Treinamentos - Python/AULA1/codigo.py
@@ -7,6 +7,7 @@
 # Passo 5 : Repetir o cadastro para todos os produtos
 
 import pyautogui
+import time
 
 # pyautogui.click -> clicar com o mouse
 # pyautogui.write -> escrever um texto
@@ -14,5 +15,27 @@ import pyautogui
 # pyautogui.hotkey -> atalho (combinação de teclas)
 
 # pyautogui.press("win") -> Vai abrir a janelinha
+
+# Passo 1
+
+pyautogui.PAUSE = 1 # Adiciona um delay de 1 segundo
+
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write(link)
+pyautogui.press("enter")
+time.sleep(3) # Uma pausa maior para uma linha especifica
+
+
+# Sessão de login
+pyautogui.click(x=2465, y=-90)
+pyautogui.write("email123@gmail.com")
+pyautogui.press("enter")
+pyautogui.click(x=2361, y=-29)
+pyautogui.write("123")
+pyautogui.press("enter")
 
 
