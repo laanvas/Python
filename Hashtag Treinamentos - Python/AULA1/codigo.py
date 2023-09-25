fc345f6f9daaf1a5c6603c12431130d79c5d1702
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-09-25 19:55:25 -0300

    25-09-23

---
+++ Hashtag Treinamentos - Python/AULA1/codigo.py
@@ -0,0 +1,18 @@
+# Passo a passo do Projeto
+# Passo 1 : Entrar no sistema da empresa
+# https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 2 : Fazer login
+# Passo 3 : Importa a base de dados de produtos
+# Passo 4 : Cadastrar o produto
+# Passo 5 : Repetir o cadastro para todos os produtos
+
+import pyautogui
+
+# pyautogui.click -> clicar com o mouse
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> aperta 1 tecla
+# pyautogui.hotkey -> atalho (combinação de teclas)
+
+# pyautogui.press("win") -> Vai abrir a janelinha
+
+
