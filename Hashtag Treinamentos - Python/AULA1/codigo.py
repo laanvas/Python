9e14f73b908aa516b4bbcb4047444d2596f5c2a8
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-09-25 22:18:14 -0300

    25-09-23

--- Hashtag Treinamentos - Python/AULA1/codigo.py
+++ Hashtag Treinamentos - Python/AULA1/codigo.py
@@ -39,34 +39,46 @@ pyautogui.press("tab")
 pyautogui.write("123")
 pyautogui.press("enter")
 
+time.sleep(1)
 
 for linha in tabela.index:
 
     pyautogui.click(x=2437, y=-180)
 
     codigo = tabela.loc[linha, "codigo"]
+    marca = tabela.loc[linha, "marca"]
+    tipo = tabela.loc[linha, "tipo"]
+    categoria = tabela.loc[linha, "categoria"]
+    preco = tabela.loc[linha, "preco_unitario"]
+    custo = tabela.loc[linha, "custo"]
 
-    pyautogui.write(codigo)
+
+
+
+    pyautogui.write(str(codigo))
     pyautogui.press("tab")
 
-    pyautogui.write("")
+    pyautogui.write(str(marca))
     pyautogui.press("tab")
 
-    pyautogui.write("")
+    pyautogui.write(str(tipo))
     pyautogui.press("tab")
 
-    pyautogui.write("")
+    pyautogui.write(str(categoria))
     pyautogui.press("tab")
 
-    pyautogui.write("")
+    pyautogui.write(str(preco))
     pyautogui.press("tab")
 
-    pyautogui.write("")
+    pyautogui.write(str(custo))
     pyautogui.press("tab")
 
-    pyautogui.write("")
-    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+         pyautogui.write(str(obs))
 
+    
+    pyautogui.press("tab")
     pyautogui.press("enter") # apertar para enviar
 
 
