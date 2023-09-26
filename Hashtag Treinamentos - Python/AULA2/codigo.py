28934ea4920e5ddc95a548c6bdae350bdae53292
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-09-26 19:53:00 -0300

    26-09-23

---
+++ Hashtag Treinamentos - Python/AULA2/codigo.py
@@ -0,0 +1,15 @@
+# Passo a Passo:
+# Passo 1: Importar a base de dados
+# Passo 2: Visualizar a base de dados
+# Passo 3: Corrigir erros da base de dados
+# Passo 4: Primeira análise do cancelamento dos clientes (Qual o % de clientes que cancelou)
+# Passo 5: Análise a causa do cancelamento dos clientes
+
+import pandas #importando a bliblioteca do pandas
+
+tabela = pandas.read_csv("cancelamentos_sample.csv") #declarando a tabela como uma variavel
+
+#print(tabela) -> comando para visualizar a base de dados
+
+
+
