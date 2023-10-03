03d92847b9914a0750bb5544d2cfad6d88bf8708
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-10-03 18:44:11 -0300

    03-10-23

--- Assistente Pessoal/jarvina.py
+++ Assistente Pessoal/jarvina.py
@@ -1,36 +1,15 @@
 import speech_recognition as sr
 
 # obtain audio from the microphone
-r = sr.Recognizer()
+microfone = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+    print("Aguardando o Comando")
+    audio = microfone.listen(source)
 
-# recognize speech using Sphinx
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    print("Google Speech Recognition thinks you said " + microfone.recognize_google(audio, language='pt-BR'))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-# recognize speech using Google Cloud Speech
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-try:
-    print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
