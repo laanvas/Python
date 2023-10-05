6c01229e10187138b8482ba5cc357beb9a0af30a
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-10-05 08:52:14 -0300

    05-10-23

--- Assistente Pessoal/jarvina.py
+++ Assistente Pessoal/jarvina.py
@@ -1,15 +1,18 @@
 import speech_recognition as sr
 
 # obtain audio from the microphone
-microfone = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Aguardando o Comando")
-    audio = microfone.listen(source)
 
-try:
+def monitora_microfone():
+    microfone = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Aguardando o Comando")
+        audio = microfone.listen(source)
 
-    print("Google Speech Recognition thinks you said " + microfone.recognize_google(audio, language='pt-BR'))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    try:
+
+        print(microfone.recognize_google(audio, language='pt-BR'))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
